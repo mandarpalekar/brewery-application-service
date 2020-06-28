1105975332153984301405b1f3d3500b76dfe708
Mandar Palekar <mpalekar@tnsi.com>
2020-06-28 10:52:48 +0930

    initial setup for service. closes #1

---
+++ src/main/java/com/mandar/brewery/breweryapplicationservice/BreweryApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mandar.brewery.breweryapplicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplicationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BreweryApplicationServiceApplication.class, args);
+  }
+
+}
